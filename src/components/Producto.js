f0cf8d7abbaee68c8b6ce26afa47260d209d8a0e
TonySauceda <asauceda@boz.mx>
2021-09-26 20:33:28 -0500

    Agregar boton y estilos

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,13 +1,28 @@
 import { Component } from 'react'
+import Button from './Button'
+
+const styles = {
+	product: {
+		border: 'solid 1px #eee',
+		boxShadow: '5px 5px 5px rgb(0,0,0,0.1)',
+		width: '30%',
+		padding: '10px 15px',
+		borderRadius: '15px',
+	},
+	img: {
+		width: '100%',
+	},
+}
 
 class Producto extends Component {
 	render() {
-		const { product } = this.props
+		const { product, addToCart } = this.props
 		return (
-			<div>
-				<img alt={product.name} src={product.img} />
+			<div style={styles.product}>
+				<img style={styles.img} alt={product.name} src={product.img} />
 				<h3>{product.name}</h3>
-				<p>{product.price}</p>
+				<p>${product.price}</p>
+				<Button onClick={() => addToCart(product)}>Agregar al carrito</Button>
 			</div>
 		)
 	}
--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,12 +1,19 @@
 import { Component } from 'react'
 import Producto from './Producto'
 
+const styles = {
+	products: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+}
 class Productos extends Component {
 	render() {
 		const { products, addToCart } = this.props
 
 		return (
-			<div>
+			<div style={styles.products}>
 				{products.map((product) => (
 					<Producto addToCart={addToCart} key={product.name} product={product} />
 				))}
