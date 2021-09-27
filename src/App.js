21e96d45539bdf9a92ca0a354e3c3e9fe1861b60
TonySauceda <asauceda@boz.mx>
2021-09-26 19:58:20 -0500

    Agregar productos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+	state = {
+		products: [
+			{ name: 'Tomate', price: 100, img: '/products/tomate.jpg' },
+			{ name: 'Arbejas', price: 200, img: '/products/arbeja.jpg' },
+			{ name: 'Lechuga', price: 300, img: '/products/lechuga.jpg' },
+		],
+	}
 	render() {
 		return (
 			<div>
